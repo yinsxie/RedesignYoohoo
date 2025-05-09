294c2236107c9287a55baecdeefe782834db4095
yinsxie <joy.kiwi31@gmail.com>
2025-05-09 13:24:47 +0700

    Initial Commit

---
+++ Redesign Yoohoo/Redesign_YoohooApp.swift
@@ -0,0 +1,17 @@
+//
+//  Redesign_YoohooApp.swift
+//  Redesign Yoohoo
+//
+//  Created by Amelia Morencia Irena on 09/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Redesign_YoohooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
